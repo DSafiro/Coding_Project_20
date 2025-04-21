91f27567f1fc38c0667649d69a9b35ad2657e446
Dylan <safirodylan@gmail.com>
2025-04-20 20:06:01 -0400

    Fixing errors and adding style

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import React, { useState } from 'react';
-import Gallery from './components/Gallery';
-import DestinationSelector from './components/DestinationSelector';
+import React, { useState } from 'react'; // Imports react and local state
+import Gallery from './components/Gallery'; // Imports gallery component
+import DestinationSelector from './components/DestinationSelector'; // Imports destination selector component
+import "./styles/styles.css"; // Imports styles
 
-function App() {
-  const [tours, setTours] = useState([]);
-  const [selectedDestination, setSelectedDestination] = useState('all');
+function App() { 
+  const [tours, setTours] = useState([]); // Initializes tours state to an empty array
+  const [selectedDestination, setSelectedDestination] = useState(''); // Initializes with empty string
 
-  const filteredTours =
-    selectedDestination === 'all'
+  const filteredTours = // Filters tours based on selected destination
+    selectedDestination === ''
       ? tours
       : tours.filter((tour) => tour.name === selectedDestination);
 
   return (
     <main>
       <h1>Our Tours</h1>
+    
+      {/* Dropdown Filter */}
       <DestinationSelector
         tours={tours}
-        selectedDestination={selectedDestination}
-        setSelectedDestination={setSelectedDestination}
+        onDestinationChange={setSelectedDestination}
+        />
+    
+      {/* Tour Gallery Display */}
+      <Gallery
+        tours={filteredTours}
+        setTours={setTours}
+        onRemove={(id) => setTours(tours.filter((tour) => tour.id !== id))}
       />
     </main>
   );
-}
+};
 
-export default App;
+export default App; // Exports app component
